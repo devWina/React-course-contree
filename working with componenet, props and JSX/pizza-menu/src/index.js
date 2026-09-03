701a247af27af5702cb699f017e533ecb3603e7a
devWina <femiadeyeye21@gmail.com>
2026-09-03 12:19:57 +0100

    feat: implement initial React pizza menu layout with images and styles

--- working with componenet, props and JSX/pizza-menu/src/index.js
+++ working with componenet, props and JSX/pizza-menu/src/index.js
@@ -1,10 +1,143 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
+
+/*  JSX RULES 
+1. JSX works like HTML, but we can enter "JS mode" by using {} (for text or attributes)
+2. We can place JS expressions inside {} e.g. reference variables, create arrays or objects, [].map, ternary operator
+3. Statements are not allowed e.g. - if/else, for, switch 
+4.JSX produces a javascript expression
+*/
+
+const pizzaData = [
+  {
+    name: 'Focaccia',
+    ingredients: 'Bread with italian olive oil and rosemary',
+    price: 6,
+    photoName: 'pizzas/focaccia.jpg',
+    soldOut: false,
+  },
+  {
+    name: 'Pizza Margherita',
+    ingredients: 'Tomato and mozarella',
+    price: 10,
+    photoName: 'pizzas/margherita.jpg',
+    soldOut: false,
+  },
+  {
+    name: 'Pizza Spinaci',
+    ingredients: 'Tomato, mozarella, spinach, and ricotta cheese',
+    price: 12,
+    photoName: 'pizzas/spinaci.jpg',
+    soldOut: false,
+  },
+  {
+    name: 'Pizza Funghi',
+    ingredients: 'Tomato, mozarella, mushrooms, and onion',
+    price: 12,
+    photoName: 'pizzas/funghi.jpg',
+    soldOut: false,
+  },
+  {
+    name: 'Pizza Salamino',
+    ingredients: 'Tomato, mozarella, and pepperoni',
+    price: 15,
+    photoName: 'pizzas/salamino.jpg',
+    soldOut: true,
+  },
+  {
+    name: 'Pizza Prosciutto',
+    ingredients: 'Tomato, mozarella, ham, aragula, and burrata cheese',
+    price: 18,
+    photoName: 'pizzas/prosciutto.jpg',
+    soldOut: false,
+  },
+];
 
 function App() {
-  return <h1>Hello React!!! </h1>;
+  return (
+    <div className='container'>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
 }
 
+// creating component
+
+function Header() {
+  const style = {};
+  return (
+    <header className='header'>
+      <h1 style={style}>Fast React Pizza Co.</h1>
+    </header>
+  );
+}
+
+function Menu() {
+  return (
+    <main className='menu'>
+      <h2>Our menu</h2>
+
+      <ul className='pizzas'>
+        {pizzaData.map((pizza) => (
+          <Pizza pizzaObj={pizza} key={pizza.name} />
+        ))}
+      </ul>
+
+      {/* <Pizza
+        name='Pizza Spinaci'
+        ingredients='Tomato, mozarella, spinach, and ricotta cheese'
+        photoName=' pizzas/spinaci.jpg'
+        price={10}
+      />
+      <Pizza
+        name={'Pizza funghi'}
+        ingredients='Tomato, mushroom, onion'
+        photoName='pizzas/funghi.jpg'
+        price={12}
+      /> */}
+    </main>
+  );
+}
+
+function Pizza(props) {
+  console.log(props);
+
+  return (
+    <li className='pizza'>
+      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+      <div>
+        <h3>{props.pizzaObj.name}</h3>
+        <p>{props.pizzaObj.ingredients}</p>
+        <span>{props.pizzaObj.price + 3}</span>
+      </div>
+    </li>
+  );
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  // console.log(isOpen);
+
+  // if (hour >= openHour && hour <= closeHour) alert("We're currently open!");
+  // else alert("We're closed");
+  return (
+    <footer className='footer'>
+      <footer>
+        {hour >= 12 && hour <= 22
+          ? `Time:${hour}:00. Welcome to our pizza store`
+          : `We're closed, We open from 12:00 till 22:00.`}
+      </footer>
+    </footer>
+  );
+}
+
+//React v18
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
